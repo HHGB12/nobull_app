841df943f3af666f891b0e3c6d631291bc7d4e7d
Rich Smith <rich@richjdsmith.ca>
2018-10-31 22:00:27 +0000

    Added some motivation!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,4 @@
 module ApplicationHelper
 
-  def calc_completeness
-    completeness = 0
-    if current_user.about_contents_complete?
-      completeness += 16.6
-    end
-    if current_user.business_details_complete?
-      completeness += 16.6
-    end
-    if current_user.integrations_complete?
-      completeness += 16.6
-    end
-    if current_user.services_contents_complete?
-      completeness += 16.6
-    end
-    if current_user.extra_pages_complete?
-      completeness += 16.6
-    end
-    if current_user.uploads_complete?
-      completeness += 16.6
-    end
-    return completeness
-  end
-
-  def is_complete?
-     calc_completeness().round == 100
-  end
+ 
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,47 @@
 module PagesHelper
+
+  def calc_completeness
+    completeness = 0
+    if current_user.about_contents_complete?
+      completeness += 16.6
+    end
+    if current_user.business_details_complete?
+      completeness += 16.6
+    end
+    if current_user.integrations_complete?
+      completeness += 16.6
+    end
+    if current_user.services_contents_complete?
+      completeness += 16.6
+    end
+    if current_user.extra_pages_complete?
+      completeness += 16.6
+    end
+    if current_user.uploads_complete?
+      completeness += 16.6
+    end
+    return completeness
+  end
+
+  def is_complete?
+     calc_completeness().round == 100
+  end
+
+  def nearly_done_message
+    case calc_completeness().round
+    when 17
+      'One down! Only five to go. 😁'
+    when 33
+      'Look at you go - thats two complete! Keep it going. Only four more to go! 🤠'
+    when 50
+      'Halfway! 🎉 Nice work. It\'s the easy one\'s left, I swear. '
+    when 66
+      'Two thirds done now - you\'re at the final push! 💪'
+    when 83
+      'Second last one. You\'ve got this! 🤗'
+    when 100
+      'YOU DID IT! 👏👏 All done! Time to celebrate! Go grab a beer or something - you\'ve earned it!'
+    end
+
+  end
 end
