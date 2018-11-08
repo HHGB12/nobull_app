40d891b3b94e5c5ca2ff7b82971b0c0efc328b35
Rich Smith <rich@richjdsmith.ca>
2018-11-08 10:57:48 +0000

    Updated devise sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_sanitized_parameters, if: :devise_controller?
   after_action :verify_authorized, unless: :devise_controller?
-  def configure_permitted_parameters
+  def configure_sanitized_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :business_name, :direct_phone_number, :has_current_website, :current_website, :city, :region, :postal_code])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :business_name, :direct_phone_number, :has_current_website, :current_website, :city, :region, :postal_code])
   end
 end
